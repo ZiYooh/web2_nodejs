a2b957884b8f52bbb2907deb11dbcc452b714f71
ZiYooh <natsucomi42@gmail.com>
2021-02-24 16:27:37 +0900

    App - 글수정 - 수정된 내용 저장

--- main.js
+++ main.js
@@ -96,7 +96,6 @@ var app = http.createServer(function(request,response){
                 response.end();
             })
         });
-
     } else if(pathname === '/update'){
         fs.readdir('./data', function(error, filelist){
             fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
@@ -120,6 +119,24 @@ var app = http.createServer(function(request,response){
                     response.end(template);
                 });
             });
+    } else if(pathname === '/update_process'){
+        var body = '';
+        
+        request.on('data', function (data){
+            body = body + data;
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            var id = post.id;
+            var title = post.title;
+            var description = post.description;
+            fs.rename(`data/${id}`, `data/${title}`, function(err){
+                fs.writeFile(`data/${title}`, description, 'utf-8', function(err){
+                    response.writeHead(302, {Location: `/?id=${title}`});
+                    response.end();
+                })
+            });
+        });
     } else {
         response.writeHead(404);
         response.end('Not found');
